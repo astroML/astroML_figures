f3dc5a6a7e049b13cbe7aefca8ba7eeb024f60bb
Brigitta Sipocz <bsipocz@gmail.com>
2019-02-07 13:07:16 -0800

    Changing the network to match the photoz example in the book

--- book_figures/appendix/fig_neural_network.py
+++ book_figures/appendix/fig_neural_network.py
@@ -30,11 +30,10 @@ circ = plt.Circle((1, 1), 2)
 
 radius = 0.3
 
-arrow_kwargs = dict(head_width=0.05, fc='black')
-
 
 # function to draw arrows
-def draw_connecting_arrow(ax, circ1, rad1, circ2, rad2):
+def draw_connecting_arrow(ax, circ1, rad1, circ2, rad2,
+                          arrow_kwargs={'head_width': 0.05, 'fc': 'black'}):
     theta = np.arctan2(circ2[1] - circ1[1],
                        circ2[0] - circ1[0])
 
@@ -53,20 +52,21 @@ def draw_circle(ax, center, radius):
     circ = plt.Circle(center, radius, fc='none', lw=2)
     ax.add_patch(circ)
 
+
 x1 = -3
 x2 = 0
 x3 = 3
 y3 = -0.75
 
-#------------------------------------------------------------
+# ------------------------------------------------------------
 # draw circles
-for i, y1 in enumerate(np.linspace(0.5, -2, 2)):
+for i, y1 in enumerate(np.linspace(1.5, -2.5, 5)):
     draw_circle(ax, (x1, y1), radius)
     ax.text(x1 - 0.9, y1, '$x_{}$'.format(i + 1),
             ha='right', va='center')
     draw_connecting_arrow(ax, (x1 - 0.9, y1), 0.1, (x1, y1), radius)
 
-for i, y2 in enumerate(np.linspace(1, -2.5, 3)):
+for i, y2 in enumerate(np.linspace(1, -2.5, 4)):
     draw_circle(ax, (x2, y2), radius)
     ax.text(x2, y2, r'$f(\theta)$', fontsize=12, ha='center', va='center')
     ax.text(x2 + 0.4, y2 * 1.1 - np.sign(y2) * 0.1, '$a_{}$'.format(i + 1))
@@ -75,24 +75,34 @@ ax.text(x3 + 0.8, y3, '$y_k$', ha='left', va='center')
 draw_connecting_arrow(ax, (x3, y3), radius, (x3 + 0.8, y3), 0.1)
 ax.text(x3, y3, r'$g(\theta)$', fontsize=12, ha='center', va='center')
 
-#------------------------------------------------------------
+# ------------------------------------------------------------
 # draw connecting arrows
-for i, y1 in enumerate(np.linspace(0.5, -2, 2)):
-    for j, y2 in enumerate(np.linspace(1, -2.5, 3)):
-        draw_connecting_arrow(ax, (x1, y1), radius, (x2, y2), radius)
-        if i % 2 == 0:
+for i, y1 in enumerate(np.linspace(1.5, -2.5, 5)):
+    for j, y2 in enumerate(np.linspace(1, -2.5, 4)):
+        # we only label a 2 sets of arrows to avoid overcrowding the figure
+        if i not in [0, 4]:
+            arrow_kwargs = {'head_width': 0.05, 'fc': 'black', 'alpha': 0.5}
+        else:
+            arrow_kwargs = {'head_width': 0.05, 'fc': 'black'}
+        draw_connecting_arrow(ax, (x1, y1), radius, (x2, y2), radius,
+                              arrow_kwargs=arrow_kwargs)
+        if i % 5 == 0:
             va = 'bottom'
             shift = 0.1
         else:
-            va='top'
+            va = 'top'
             shift = -0.1
-        ax.text((2*x1+x2)/3, (2*y1+y2)/3 + shift, '$w_{%s%s}$' % ((i+1), (j+1)), va=va, fontsize=12, ha='center')
+        if i in [0, 4]:
+            ax.text((2*x1+x2)/3, (2*y1+y2)/3 + shift,
+                    '$w_{%s%s}$' % ((i+1), (j+1)), va=va, fontsize=12,
+                    ha='center')
 
-for i, y2 in enumerate(np.linspace(1, -2.5, 3)):
+for i, y2 in enumerate(np.linspace(1, -2.5, 4)):
     draw_connecting_arrow(ax, (x2, y2), radius, (x3, y3), radius)
-    ax.text((x2+x3)/2, (y2+y3)/2 + 0.1, '$w_{%sk}$' % (i+1), fontsize=12, ha='center', va='bottom')
+    ax.text((x2+x3)/2, (y2+y3)/2 + 0.1, '$w_{%sk}$' % (i+1),
+            fontsize=12, ha='center', va='bottom')
 
-#------------------------------------------------------------
+# ------------------------------------------------------------
 # Add text labels
 plt.text(x1, 2.7, "Input Layer", ha='center', va='top')
 plt.text(x2, 2.7, r"Hidden Layer\\\\ $a_j=f(\sum_{i=1}^N w_{ij} x_i)$",
