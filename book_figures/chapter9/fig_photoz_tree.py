3bd0aef21a84f97a9da2991bbac2b85fddb819ba
Andrew Connolly <ajc@astro.washington.edu>
2019-02-11 13:56:55 -0800

    density plots for figures

--- book_figures/chapter9/fig_photoz_tree.py
+++ book_figures/chapter9/fig_photoz_tree.py
@@ -91,7 +91,11 @@ ax.legend(loc=1)
 
 # second panel: best-fit results
 ax = fig.add_subplot(122)
-ax.scatter(z_test, z_fit_best, s=1, lw=0, c='k')
+edges = np.linspace(z_test.min(), z_test.max(), 101)
+H, zs_bins, zp_bins = np.histogram2d(z_test, z_fit_best, bins=edges)
+ax.imshow(H.T, origin='lower', interpolation='nearest', aspect='auto', 
+           extent=[zs_bins[0], zs_bins[-1], zs_bins[0], zs_bins[-1]],
+           cmap=plt.cm.binary)
 ax.plot([-0.1, 0.4], [-0.1, 0.4], ':k')
 ax.text(0.04, 0.96, "depth = %i\nrms = %.3f" % (best_depth, rms_test[i_best]),
         ha='left', va='top', transform=ax.transAxes)
