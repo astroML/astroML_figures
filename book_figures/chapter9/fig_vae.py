a2d0989bb2320b79d2acc5be0a36814a82931f1b
Brigitta Sipocz <bsipocz@gmail.com>
2019-02-12 01:33:35 -0800

    Minor tweeks to VAE figure after review

--- book_figures/chapter9/fig_vae.py
+++ book_figures/chapter9/fig_vae.py
@@ -12,15 +12,17 @@ Variational AutoEncoder
 
 import numpy as np
 from matplotlib import pyplot as plt
-
-#----------------------------------------------------------------------
+import matplotlib
+# ----------------------------------------------------------------------
 # This function adjusts matplotlib settings for a uniform feel in the textbook.
 # Note that with usetex=True, fonts are rendered with LaTeX.  This may
 # result in an error if LaTeX is not installed on your system.  In that case,
 # you can set usetex to False.
-if "setup_text_plots" not in globals():
-    from astroML.plotting import setup_text_plots
-setup_text_plots(fontsize=12, usetex=True)
+from astroML.plotting import setup_text_plots
+# uselatex need to be `False` to be able to use bold fonts for text
+setup_text_plots(fontsize=12, usetex=False)
+
+matplotlib.rc('font', weight='bold')
 
 fig = plt.figure(figsize=(6, 4), facecolor='w')
 ax = fig.add_axes([0, 0, 1, 1],
@@ -31,6 +33,7 @@ circ = plt.Circle((1, 1), 2)
 radius = 0.3
 
 
+# ----------------------------------------------------------------------
 # function to draw arrows
 def draw_connecting_arrow(ax, circ1, rad1, circ2, rad2,
                           arrow_kwargs={'head_width': 0.05, 'fc': 'black',
@@ -50,7 +53,7 @@ def draw_connecting_arrow(ax, circ1, rad1, circ2, rad2,
 
 # function to draw circles
 def draw_circle(ax, center, radius, **kwargs):
-    circ = plt.Circle(center, radius, fc='none', lw=2, **kwargs)
+    circ = plt.Circle(center, radius, fc='none', lw=1, **kwargs)
     ax.add_patch(circ)
 
 
@@ -63,7 +66,7 @@ x6 = 3.5
 
 seq1 = np.linspace(2.5, -2, 4)
 seq2 = np.linspace(1.75, -1.5, 3)
-seq3 = np.hstack([np.linspace(2.5, 1, 3), np.linspace(-0.5, -2, 3)])
+seq3 = np.hstack([np.linspace(2.5, 1, 2), np.linspace(-0.5, -2, 2)])
 seq4 = np.linspace(1, -0.5, 3)
 seq5 = np.linspace(1.75, -1.5, 3)
 seq6 = np.linspace(2.5, -2, 4)
@@ -80,26 +83,29 @@ for i, y2 in enumerate(seq2):
 for i, y3 in enumerate(seq3):
     draw_circle(ax, (x3, y3), radius * 0.75)
 
-ax.add_patch(plt.Rectangle((x3-radius, seq3[2]-radius), radius * 2,
-                           seq3[0]-seq3[2] + 2 * radius, fc='b', alpha=0.5))
-ax.text(x3, seq3[1], r'$\sigma$', fontsize=12, ha='center', va='center')
+ax.add_patch(plt.Rectangle((x3 - radius, seq3[1] - radius), radius * 2,
+                           seq3[0] - seq3[1] + 2 * radius, fc='b', alpha=0.5))
+ax.text(x3, (seq3[0] + seq3[1]) / 2, r'$\sigma$', fontsize=12,
+        ha='center', va='center')
 
-ax.add_patch(plt.Rectangle((x3-radius, seq3[5]-radius), radius * 2,
-                           seq3[3]-seq3[5] + 2 * radius, fc='y', alpha=0.5))
-ax.text(x3, seq3[4], r'$\mu$', fontsize=12, ha='center', va='center')
+ax.add_patch(plt.Rectangle((x3 - radius, seq3[3] - radius), radius * 2,
+                           seq3[2] - seq3[3] + 2 * radius, fc='y', alpha=0.5))
+ax.text(x3, (seq3[2] + seq3[3]) / 2, r'$\mu$', fontsize=12,
+        ha='center', va='center')
 
-draw_connecting_arrow(ax, (x3 + radius, seq3[1]), radius * 0.15,
+draw_connecting_arrow(ax, (x3 + radius, (seq3[0] + seq3[1]) / 2), radius * 0.15,
                       (x4 - radius, seq4[1]), radius * 0.2)
-draw_connecting_arrow(ax, (x3 + radius, seq3[4]), radius * 0.15,
+draw_connecting_arrow(ax, (x3 + radius, (seq3[2] + seq3[3]) / 2), radius * 0.15,
                       (x4 - radius, seq4[1]), radius * 0.2)
 
 for i, y4 in enumerate(seq4):
     draw_circle(ax, (x4, y4), radius * 0.75, alpha=0.5)
 
-ax.text(x4, seq4[1], 'Sample', fontsize=12, ha='center', va='center', rotation=90)
+ax.text(x4, seq4[1], 'Sample', fontsize=12, ha='center',
+        va='center', rotation=90)
 
-ax.add_patch(plt.Rectangle((x4-radius, seq4[2]-radius), radius * 2,
-                           seq4[0]-seq4[2] + 2 * radius, fc='g', alpha=0.5))
+ax.add_patch(plt.Rectangle((x4 - radius, seq4[2] - radius), radius * 2,
+                           seq4[0] - seq4[2] + 2 * radius, fc='g', alpha=0.5))
 
 for i, y5 in enumerate(seq5):
     draw_circle(ax, (x5, y5), radius)
@@ -130,10 +136,10 @@ for i, y5 in enumerate(seq5):
 # ------------------------------------------------------------
 # Add axis
 
-ax.arrow(-3.8, -2.6, 0, 5, head_width=0.05)
-ax.arrow(-3.8, -2.6, 7.5, 0, head_width=0.05)
-plt.text(0, -2.8, "Latent Space 1", ha='center', va='center')
-plt.text(-3.9, 0, "Latent Space 2", ha='center', va='center', rotation=90)
+#ax.arrow(-3.8, -2.6, 0, 5, head_width=0.05)
+#ax.arrow(-3.8, -2.6, 7.5, 0, head_width=0.05)
+#plt.text(0, -2.8, "Latent Space 1", ha='center', va='center')
+#plt.text(-3.9, 0, "Latent Space 2", ha='center', va='center', rotation=90)
 
 
 ax.set_aspect('equal')
